f345dbe571378dc2ee10553ab943d8b544cdca64
456vv <123@456vv.com>
2016-08-21 14:36:36 +0800

    Add files via upload

--- v1/test/d2d-main.go
+++ v1/test/d2d-main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"github.com/456vv/vforward/v1"
-    "flag"
     "net"
-    "time"
     "log"
+    "time"
+    "flag"
     "fmt"
 )
-var fNetwork = flag.String("Network", "tcp", "网络地址类型")
-var fALocal = flag.String("ALocal", "", "A本地监听网卡IP地址 (format \"12.13.14.15:123\")")
-var fBLocal = flag.String("BLocal", "", "B本地监听网卡IP地址 (format \"22.23.24.25:234\")")
 
+var fNetwork = flag.String("Network", "tcp", "网络地址类型")
+
+var fALocal = flag.String("ALocal", "0.0.0.0", "A端本地发起连接地址")
+var fALemote = flag.String("ALemote", "", "A端远程请求连接地址 (format \"12.13.14.15:123\")")
+
+var fBLocal = flag.String("BLocal", "0.0.0.0", "B端本地发起连接地址")
+var fBLemote = flag.String("BLemote", "", "B端远程请求连接地址 (format \"22.23.24.25:234\")")
+
+var fTryConnTime = flag.Duration("TryConnTime", time.Millisecond*500, "尝试或发起连接时间，可能一方不在线，会一直尝试连接对方。单位：ns, us, ms, s, m, h")
 var fTimeout = flag.Duration("Timeout", time.Second*5, "转发连接时候，请求远程连接超时。单位：ns, us, ms, s, m, h")
 var fMaxConn = flag.Int("MaxConn", 500, "限制连接最大的数量")
 var fKeptIdeConn = flag.Int("KeptIdeConn", 2, "保持一方连接数量，以备快速互相连接。")
 var fReadBufSize = flag.Int("ReadBufSize", 4096, "交换数据缓冲大小。单位：字节")
 
 
-//commandline:l2l-main.exe -ALocal 127.0.0.1:1201 -BLocal 127.0.0.1:1202
+//commandline:d2d-main.exe -ALemote 127.0.0.1:1201 -BLemote 127.0.0.1:1202
 func main(){
     flag.Parse()
     if flag.NFlag() == 0 {
@@ -26,34 +32,41 @@ func main(){
         return
     }
     var err error
-    if *fALocal == "" || *fBLocal == "" {
-        log.Printf("地址未填，A监听地址 %q, B监听地址 %q", *fALocal, *fBLocal)
+    if *fALemote == "" || *fBLemote == "" {
+        log.Printf("地址未填，A端远程地址 %q, B端远程地址 %q", *fALemote, *fBLemote)
         return
     }
-    addr1, err := net.ResolveTCPAddr(*fNetwork, *fALocal)
+    rtcpaddr1, err := net.ResolveTCPAddr(*fNetwork, *fALemote)
     if err != nil {
         log.Println(err)
         return
     }
-    addr2, err := net.ResolveTCPAddr(*fNetwork, *fBLocal)
+    rtcpaddr2, err := net.ResolveTCPAddr(*fNetwork, *fBLemote)
     if err != nil {
         log.Println(err)
         return
     }
+
     addra := &vforward.Addr{
         Network:*fNetwork,
-        Local: addr1,
+        Local: &net.TCPAddr{IP: net.ParseIP(*fALocal),Port: 0,},
+        Remote: rtcpaddr1,
     }
     addrb := &vforward.Addr{
         Network:*fNetwork,
-        Local: addr2,
+        Local: &net.TCPAddr{IP: net.ParseIP(*fBLocal),Port: 0,},
+        Remote: rtcpaddr2,
     }
-    ll := &vforward.L2L{
+
+	dd := &vforward.D2D{
+        TryConnTime: *fTryConnTime,             // 尝试或发起连接时间，可能一方不在线，会一直尝试连接对方。
         MaxConn: *fMaxConn,                     // 限制连接最大的数量
         KeptIdeConn: *fKeptIdeConn,             // 保持一方连接数量，以备快速互相连接。
+        Timeout: *fTimeout,                     // 发起连接超时
         ReadBufSize: *fReadBufSize,             // 交换数据缓冲大小
     }
-    lls, err := ll.Transport(addra, addrb)
+    defer dd.Close()
+    dds, err := dd.Transport(addra, addrb)
     if err != nil {
         log.Println(err)
         return
@@ -61,26 +74,26 @@ func main(){
     exit := make(chan bool, 1)
     go func(){
         defer func(){
-            lls.Close()
+            dds.Close()
             exit <- true
             close(exit)
         }()
-        log.Println("L2L启动了")
+        log.Println("D2D启动了")
 
         var in0 string
         for err == nil  {
-            log.Println("输入任何字符可以退出L2D!")
+            log.Println("输入任何字符可以退出D2D!")
             fmt.Scan(&in0)
             if in0 != "" {
                 return
             }
         }
     }()
-    defer lls.Close()
-    err = lls.Swap()
+    defer dds.Close()
+    err = dds.Swap()
     if err != nil {
         log.Println("错误：%s", err)
     }
     <-exit
-    log.Println("L2L退出了")
+    log.Println("D2D退出了")
 }
--- v1/test/l2d-main.go
+++ v1/test/l2d-main.go
@@ -8,17 +8,19 @@ import (
     "log"
     "fmt"
 )
+
 var fNetwork = flag.String("Network", "tcp", "网络地址类型")
-var fALocal = flag.String("ALocal", "", "A本地监听网卡IP地址 (format \"12.13.14.15:123\")")
-var fBLocal = flag.String("BLocal", "", "B本地监听网卡IP地址 (format \"22.23.24.25:234\")")
+
+var fListen = flag.String("Listen", "", "本地网卡监听地址 (format \"0.0.0.0:123\")")
+
+var fFromLocal = flag.String("FromLocal", "0.0.0.0", "转发请求的源地址")
+var fToLemote = flag.String("ToLemote", "", "转发请求的目地址 (format \"22.23.24.25:234\")")
 
 var fTimeout = flag.Duration("Timeout", time.Second*5, "转发连接时候，请求远程连接超时。单位：ns, us, ms, s, m, h")
 var fMaxConn = flag.Int("MaxConn", 500, "限制连接最大的数量")
-var fKeptIdeConn = flag.Int("KeptIdeConn", 2, "保持一方连接数量，以备快速互相连接。")
 var fReadBufSize = flag.Int("ReadBufSize", 4096, "交换数据缓冲大小。单位：字节")
 
-
-//commandline:l2l-main.exe -ALocal 127.0.0.1:1201 -BLocal 127.0.0.1:1202
+//commandline:l2d-main.exe -Listen 127.0.0.1:1201 -ToLemote 127.0.0.1:1202
 func main(){
     flag.Parse()
     if flag.NFlag() == 0 {
@@ -26,34 +28,39 @@ func main(){
         return
     }
     var err error
-    if *fALocal == "" || *fBLocal == "" {
-        log.Printf("地址未填，A监听地址 %q, B监听地址 %q", *fALocal, *fBLocal)
+    if *fListen == "" || *fToLemote == "" {
+        log.Printf("地址未填，本地监听地址 %q, 转发到远程地址 %q", *fListen, *fToLemote)
         return
     }
-    addr1, err := net.ResolveTCPAddr(*fNetwork, *fALocal)
+
+    listenIP, err := net.ResolveTCPAddr(*fNetwork, *fListen)
     if err != nil {
         log.Println(err)
         return
     }
-    addr2, err := net.ResolveTCPAddr(*fNetwork, *fBLocal)
+    dialIP, err := net.ResolveTCPAddr(*fNetwork, *fToLemote)
     if err != nil {
         log.Println(err)
         return
     }
-    addra := &vforward.Addr{
+
+    dial := &vforward.Addr{
         Network:*fNetwork,
-        Local: addr1,
+        Local: &net.TCPAddr{IP: net.ParseIP(*fFromLocal),Port: 0,},
+        Remote: dialIP,
     }
-    addrb := &vforward.Addr{
+    listen := &vforward.Addr{
         Network:*fNetwork,
-        Local: addr2,
+        Local: listenIP,
     }
-    ll := &vforward.L2L{
-        MaxConn: *fMaxConn,                     // 限制连接最大的数量
-        KeptIdeConn: *fKeptIdeConn,             // 保持一方连接数量，以备快速互相连接。
-        ReadBufSize: *fReadBufSize,             // 交换数据缓冲大小
+
+    ld := &vforward.L2D{
+        MaxConn: *fMaxConn,            // 限制连接最大的数量
+        ReadBufSize: *fReadBufSize,    // 交换数据缓冲大小
+        Timeout: *fTimeout,            // 发起连接超时
     }
-    lls, err := ll.Transport(addra, addrb)
+
+    lds, err := ld.Transport(dial, listen)
     if err != nil {
         log.Println(err)
         return
@@ -61,11 +68,11 @@ func main(){
     exit := make(chan bool, 1)
     go func(){
         defer func(){
-            lls.Close()
+            lds.Close()
             exit <- true
             close(exit)
         }()
-        log.Println("L2L启动了")
+        log.Println("L2D启动了")
 
         var in0 string
         for err == nil  {
@@ -76,11 +83,11 @@ func main(){
             }
         }
     }()
-    defer lls.Close()
-    err = lls.Swap()
+    defer lds.Close()
+    err = lds.Swap()
     if err != nil {
         log.Println("错误：%s", err)
     }
     <-exit
-    log.Println("L2L退出了")
+    log.Println("L2D退出了")
 }
