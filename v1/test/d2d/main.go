61bf62913e5bc4433e6b7c397e89c02ba1270f8a
456vv <123@456vv.com>
2016-08-21 15:55:07 +0800

    Add files via upload

--- v1/test/d2d/main.go
+++ v1/test/d2d/main.go
@@ -12,10 +12,10 @@ import (
 var fNetwork = flag.String("Network", "tcp", "网络地址类型")
 
 var fALocal = flag.String("ALocal", "0.0.0.0", "A端本地发起连接地址")
-var fALemote = flag.String("ALemote", "", "A端远程请求连接地址 (format \"12.13.14.15:123\")")
+var fARemote = flag.String("ARemote", "", "A端远程请求连接地址 (format \"12.13.14.15:123\")")
 
 var fBLocal = flag.String("BLocal", "0.0.0.0", "B端本地发起连接地址")
-var fBLemote = flag.String("BLemote", "", "B端远程请求连接地址 (format \"22.23.24.25:234\")")
+var fBRemote = flag.String("BRemote", "", "B端远程请求连接地址 (format \"22.23.24.25:234\")")
 
 var fTryConnTime = flag.Duration("TryConnTime", time.Millisecond*500, "尝试或发起连接时间，可能一方不在线，会一直尝试连接对方。单位：ns, us, ms, s, m, h")
 var fTimeout = flag.Duration("Timeout", time.Second*5, "转发连接时候，请求远程连接超时。单位：ns, us, ms, s, m, h")
@@ -24,7 +24,7 @@ var fKeptIdeConn = flag.Int("KeptIdeConn", 2, "保持一方连接数量，以备
 var fReadBufSize = flag.Int("ReadBufSize", 4096, "交换数据缓冲大小。单位：字节")
 
 
-//commandline:d2d-main.exe -ALemote 127.0.0.1:1201 -BLemote 127.0.0.1:1202
+//commandline:d2d-main.exe -ARemote 127.0.0.1:1201 -BRemote 127.0.0.1:1202 -Network udp
 func main(){
     flag.Parse()
     if flag.NFlag() == 0 {
@@ -32,30 +32,43 @@ func main(){
         return
     }
     var err error
-    if *fALemote == "" || *fBLemote == "" {
-        log.Printf("地址未填，A端远程地址 %q, B端远程地址 %q", *fALemote, *fBLemote)
-        return
-    }
-    rtcpaddr1, err := net.ResolveTCPAddr(*fNetwork, *fALemote)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    rtcpaddr2, err := net.ResolveTCPAddr(*fNetwork, *fBLemote)
-    if err != nil {
-        log.Println(err)
+    if *fARemote == "" || *fBRemote == "" {
+        log.Printf("地址未填，A端远程地址 %q, B端远程地址 %q", *fARemote, *fBRemote)
         return
     }
 
-    addra := &vforward.Addr{
-        Network:*fNetwork,
-        Local: &net.TCPAddr{IP: net.ParseIP(*fALocal),Port: 0,},
-        Remote: rtcpaddr1,
-    }
-    addrb := &vforward.Addr{
-        Network:*fNetwork,
-        Local: &net.TCPAddr{IP: net.ParseIP(*fBLocal),Port: 0,},
-        Remote: rtcpaddr2,
+    var addra *vforward.Addr
+    var addrb *vforward.Addr
+    switch *fNetwork {
+    	case "tcp", "tcp4", "tcp6":
+            rtcpaddr1, err := net.ResolveTCPAddr(*fNetwork, *fARemote)
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            rtcpaddr2, err := net.ResolveTCPAddr(*fNetwork, *fBRemote)
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            addra = &vforward.Addr{Network:*fNetwork,Local: &net.TCPAddr{IP: net.ParseIP(*fALocal),Port: 0,},Remote: rtcpaddr1,}
+            addrb = &vforward.Addr{Network:*fNetwork,Local: &net.TCPAddr{IP: net.ParseIP(*fBLocal),Port: 0,},Remote: rtcpaddr2,}
+    	case "udp", "udp4", "udp6":
+            rudpaddr1, err := net.ResolveUDPAddr(*fNetwork, *fARemote)
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            rudpaddr2, err := net.ResolveUDPAddr(*fNetwork, *fBRemote)
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            addra = &vforward.Addr{Network:*fNetwork,Local: &net.UDPAddr{IP: net.ParseIP(*fALocal),Port: 0,},Remote: rudpaddr1,}
+            addrb = &vforward.Addr{Network:*fNetwork,Local: &net.UDPAddr{IP: net.ParseIP(*fBLocal),Port: 0,},Remote: rudpaddr2,}
+        default:
+            log.Printf("网络地址类型  %q 是未知的，日前仅支持：tcp/tcp4/tcp6 或 upd/udp4/udp6", *fNetwork)
+            return
     }
 
 	dd := &vforward.D2D{
@@ -82,7 +95,7 @@ func main(){
 
         var in0 string
         for err == nil  {
-            log.Println("输入任何字符可以退出D2D!")
+            log.Println("输入任何字符，并回车可以退出L2D!")
             fmt.Scan(&in0)
             if in0 != "" {
                 return
